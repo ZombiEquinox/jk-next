c343e855e37f4174de8b9f5ed544aa7e939de43a
ZombiEquinox <chaoticbuddha@gmail.com>
2021-12-01 11:23:44 -0600

    added interactive page

--- pages/interactive.js
+++ pages/interactive.js
@@ -1,15 +1,21 @@
 import Layout from "../components/layout"
-import Link from "next/link"
 import Head from 'next/head'
+import Card from '../components/card'
 
 export default function Interactive(){
+  let width = 600;
+  let height = 600;
   return(
     <Layout>
       <Head>
         <title>Interactive Art | Jonathan A Klein</title>
       </Head>
-      My Interactive Art
-      <Link href="/interactive/work-one">Painting One</Link>
+      <h1>Interactive Art</h1>
+      <div className="flex justify-center item-stretch flex-wrap">
+        <Card link="/interactive/interactiveOne" title="Light Composition I" text="View Description" img='https://jk-docs.s3.us-east-2.amazonaws.com/images/thumb/interactive/sculpture1.jpg' width={width} height={height}/>
+        <Card link="/interactive/interactiveTwo" title="Girls got Rhythm" text="View Description" img='https://jk-docs.s3.us-east-2.amazonaws.com/images/thumb/interactive/sculpture2-1.jpg' width={width} height={height}/>
+        <Card link="/interactive/interactiveThree" title="Contoured Analytics" text="View Description" img='https://jk-docs.s3.us-east-2.amazonaws.com/images/thumb/interactive/sculpture4.jpg' width={width} height={height}/>
+      </div>
     </Layout>
   )
 }
---
+++ pages/interactive/interactiveTwo.js
@@ -0,0 +1,30 @@
+import Layout from '../../components/layout'
+import Head from 'next/head'
+import Image from 'next/image'
+import Link from 'next/link'
+
+export default function interactiveOne(){
+  return(
+    <Layout>
+      <Head>
+        <title>Girls got Rhythm | Jonathan A Klein</title>
+      </Head>
+      <Link href="/interactive">&lt; Back</Link>
+      <div>
+        <h1>Girls got Rhythm</h1>
+        <p>Controlled by an arduino and motors, this "instrument" creates it's own music, keeping a beat, and playing "notes". When presented it was accompanied by Avenged Sevenfold, then Other Music, while no two songs were alike, it kept the beat and started to "play" along. The idea behind it is music is a universal language. While some don't interpret the same song the same way, we all hear or feel it the same. this way meant to allow the listener to hear it's music, and allow them to make sense of how they hear it.</p>
+        <iframe src="//player.vimeo.com/video/36628708" webkitallowfullscreen="" mozallowfullscreen="" allowfullscreen="" width="500" height="281" frameBorder="0"></iframe>
+        <Image src="https://jk-docs.s3.us-east-2.amazonaws.com/images/interactive/sculpture2-1.jpg" width="604px" height="453px" />
+        <Image src="https://jk-docs.s3.us-east-2.amazonaws.com/images/interactive/sculpture2-2.jpg" width="604px" height="453px" />
+        <Image src="https://jk-docs.s3.us-east-2.amazonaws.com/images/interactive/sculpture2-3.jpg" width="604px" height="453px" />
+        <Image src="https://jk-docs.s3.us-east-2.amazonaws.com/images/interactive/sculpture2-4.jpg" width="604px" height="453px" />
+        <Image src="https://jk-docs.s3.us-east-2.amazonaws.com/images/interactive/sculpture2-5.jpg" width="604px" height="453px" />
+        <Image src="https://jk-docs.s3.us-east-2.amazonaws.com/images/interactive/sculpture2-6.jpg" width="604px" height="453px" />
+        <Image src="https://jk-docs.s3.us-east-2.amazonaws.com/images/interactive/sculpture2-7.jpg" width="604px" height="453px" />
+        <Image src="https://jk-docs.s3.us-east-2.amazonaws.com/images/interactive/sculpture2-8.jpg" width="604px" height="453px" />
+        <Image src="https://jk-docs.s3.us-east-2.amazonaws.com/images/interactive/sculpture2-9.jpg" width="604px" height="453px" />
+        <Image src="https://jk-docs.s3.us-east-2.amazonaws.com/images/interactive/sculpture2-10.jpg" width="604px" height="453px" />
+      </div>
+    </Layout>
+  )
+}
